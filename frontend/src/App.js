43826412e2e9e974145039914a133252cc5db6be
Rahul Joshi <rahuljoshi@Rahuls-MacBook-Air.local>
2026-04-26 18:39:59 +0530

    deplying changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CaseStudy from "./pages/CaseStudy";
 
 function App() {
   return (
     <div className="page">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work/wellington-hub" element={<CaseStudy />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
